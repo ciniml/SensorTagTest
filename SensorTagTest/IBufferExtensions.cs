b223c03830f51e84e3fecb385f3a6cf260f942da
Kenta IDA <cinimlm@gmail.com>
2014-08-03 05:19:05 +0900

    Initial commit

---
+++ SensorTagTest/IBufferExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage.Streams;
+
+namespace SensorTagTest
+{
+    static class IBufferExtensions
+    {
+        public static string DecodeUtf8String(this IBuffer buffer)
+        {
+            var data = buffer.ToArray();
+            return Encoding.UTF8.GetString(data);
+        }
+
+        public static ulong DecodeUint40(this IBuffer buffer)
+        {
+            var data = buffer.ToArray();
+            var decoded = data.Aggregate(0ul, (l, r) => (l << 8) | r);
+
+            return decoded;
+        }
+    }
+}
